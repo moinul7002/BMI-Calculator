483cd07ebdb1c05d9c57caeab5c6d66e8915b3f7
Md. Moinul Islam <49118924+moinul7002@users.noreply.github.com>
2020-05-23 16:53:51 +0600

    Create bmi.py

---
+++ bmi.py
@@ -0,0 +1,31 @@
+# Baseball player's Height, Weight and BMI Calculation
+
+# Dataset: http://wiki.stat.ucla.edu/socr/index.php/SOCR_Data_MLB_HeightsWeights
+
+# Import numpy
+import numpy as np
+
+# Create a numpy array from height_in: np_height_in
+np_height_in = np.array(height_in)
+
+# Print out np_height_in
+print(np_height_in)
+
+# Convert np_height_in to m: np_height_m
+np_height_m = np_height_in * 0.0254
+
+# Print np_height_m
+print(np_height_m)
+
+# Create array from weight_lb with metric units: np_weight_kg
+np_weight_kg = np.array(weight_lb) * 0.453592
+
+# Calculate the BMI: bmi
+bmi = np_weight_kg / np_height_m ** 2
+
+# Print out bmi
+print(bmi)
+
+# Lightweight baseball players
+# Create the light array
+light = bmi < 21
